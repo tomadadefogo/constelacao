fc7a04d96ab38946dc3e35862cc2c03149562f16
Pedro Luiz <pedrobkluiz@hotmail.com>
2023-01-03 02:08:27 -0300

    testando botao

---
+++ componente/Botao.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Botao.module.css'
+
+export const Botao = () => {
+  return (
+    <button className={styles.Botao}>Clique Aqui</button>
+  )
+}
---
+++ constelacao/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Botao } from '../../componente/Botao';
+
+
+const App = () => {
+  return (
+    <div>
+      <Botao />
+    </div>
+  )
+}
+
+export default App
